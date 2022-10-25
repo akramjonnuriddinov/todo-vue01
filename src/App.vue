66c81fe11776f35f4965dda207ad4ef6b786cf97
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2022-10-25 23:49:01 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import PenIcon from './components/icons/PenIcon.vue'
+import XmarkIcon from './components/icons/XmarkIcon.vue'
+import { ref, onMounted, watch } from 'vue'
+
+const input = ref('')
+const todos = ref([])
+let disabled = ref(1)
+
+const addBtn = () => {
+  if(input.value !== '') {
+    todos.value.push(input.value)
+    input.value = ''
+  }
+}
+
+const removeBtn = (todo) => {
+  todos.value = todos.value.filter((i) => i !== todo)
+}
+
+const editBtn = () => {
+  disabled = (disabled + 1) % 2
+}
+
+watch(todos, (newValue) => {
+  localStorage.setItem('todos', JSON.stringify(newValue))
+}, {
+  deep: true
+})
+
+
+onMounted(() => {
+  todos.value = JSON.parse(localStorage.getItem('todos'))
+})
+</script>
+
+<template>
+  <div class="bg-white container mx-auto px-5 my-5 max-w-2xl py-5">
+    <h1 class="text-2xl font-bold text-center mb-4">TODO LIST</h1>
+    <div class="flex justify-between mb-5">
+      <input v-model="input" class="border px-2 py-2 w-full text-xl mr-4" type="text" placeholder="Enter the task">
+      <button @click="addBtn" class="bg-blue-500 text-white text-3xl px-4 hover:bg-blue-400">+</button>
+    </div>
+    <ul>
+      <li v-for="todo in todos" class="flex justify-between items-center py-2 border-t border-b px-2 hover:bg-sky-100">
+        <label for="checked" class="flex items-center w-full cursor-pointer">
+          <input class="mr-4" type="checkbox" name="checked">
+          <input class="w-full outline-none bg-transparent text-xl text-gray-800" :disabled="disabled == 1" type="text" :value="todo">
+        </label>
+        <div class="flex items-center">
+          <button @click="disabled = (disabled + 1) % 2" class="mr-4">
+            <PenIcon class="w-4 text-sky-700" />
+          </button>
+          <button @click="removeBtn(todo)">
+            <XmarkIcon class="w-4 text-sky-700" />
+          </button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: rgb(35, 162, 235);
+}
+
+input:checked + input {
+  text-decoration: line-through;
+}
+</style>
